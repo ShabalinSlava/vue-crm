8b9824f767639fde7a32f639d84c9bf1f7ffa83a
Shabalin Slava <svaznoy0309@gmail.com>
2020-05-21 14:23:01 +0800

    create layouts and views

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,55 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('./../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('./../views/Categories.vue')
+  },
+  {
+    path: '/detailrecord',
+    name: 'detailrecord',
+    meta: {layout: 'main'},
+    component: () => import('./../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('./../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('./../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('./../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('./../views/Record.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('./../views/Register.vue')
+  },
 ];
 
 const router = new VueRouter({
