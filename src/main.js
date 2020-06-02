1601e217123971e43621d924664c85ca4d3a9bcf
Shabalin Slava <svaznoy0309@gmail.com>
2020-06-02 12:39:58 +0800

    pagination

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 
 import Vue from "vue";
 import Vuelidate from 'vuelidate';
+import Paginate from 'vuejs-paginate';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -22,6 +23,7 @@ Vue.use(Vuelidate);
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
 Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
 Vue.directive('tooltip', tooltipDirective)
 
 
